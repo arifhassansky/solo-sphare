2470907db8b420bda07e157387a01a7aec922382
arifhassansky <arifskypro@gmail.com>
2024-12-15 18:01:09 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { useLoaderData } from "react-router-dom";
+import Carousel from "../components/Carousel";
+import TabCategories from "../components/TabCategories";
+
+const Home = () => {
+  const jobs = useLoaderData();
+  return (
+    <div>
+      <Carousel />
+      <TabCategories jobs={jobs} />
+    </div>
+  );
+};
+
+export default Home;
